b7d67773f376bb074323c5191e348cb95d021fc8
Edwin Svensson <git@olback.net>
2019-10-25 02:13:35 +0200

    add support for easyeda & kicad

--- src/format/extractor_prelude.rs
+++ src/format/extractor_prelude.rs
@@ -7,7 +7,21 @@ pub(super) use std::{io::Read, path::PathBuf};
 pub type Files = HashMap::<String, Vec<u8>>;
 
 pub trait ExtractorTrait {
-    fn extract<S: Into<String>>(files: &mut Files, filepath: S, file: &mut ZipFile) -> LLResult<()>;
+    fn extract<S: Into<String>>(files: &mut Files, file_path: S, item: &mut ZipFile) -> LLResult<()>;
 }
 
+pub(super) fn generic_extractor<S: Into<String>>(match_path: &str, files: &mut Files, file_path: S, item: &mut ZipFile) -> LLResult<()> {
 
+    let fp = file_path.into();
+
+    if fp.to_lowercase().contains(match_path) {
+        let path = PathBuf::from(fp);
+        let base_name = path.file_name().unwrap().to_string_lossy().to_string();
+        let mut f_data = Vec::<u8>::new();
+        item.read_to_end(&mut f_data)?;
+        files.insert(base_name, f_data);
+    }
+
+    Ok(())
+
+}
