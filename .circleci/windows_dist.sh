2187e5e83dccb034cca96875fb62d030989bcaca
Edwin Svensson <git@olback.net>
2019-11-30 00:14:08 +0100

    fix paths

--- .circleci/windows_dist.sh
+++ .circleci/windows_dist.sh
@@ -2,13 +2,13 @@
 
 # https://gtk-rs.org/docs-src/tutorial/cross
 
-GTK_INSTALL_PATH="/usr/x86_64-w64-mingw32/sys-root/mingw/bin"
+GTK_INSTALL_PATH="/usr/x86_64-w64-mingw32/sys-root/mingw"
 DIST="dist"
 
 mkdir $DIST
 
 # Exe
-cp /target/x86_64-pc-windows-gnu/release/library-loader.exe $DIST/library-loader.exe
+cp ./target/x86_64-pc-windows-gnu/release/library-loader.exe $DIST/library-loader.exe
 
 # Dlls
 cp $GTK_INSTALL_PATH/bin/*.dll $DIST
