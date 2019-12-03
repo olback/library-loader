cd2af862993557ca2130834440cdf3f121c5b700
Edwin Svensson <git@olback.net>
2019-12-03 22:04:37 +0100

    fix dlls attempt 2

--- tools/get_dlls.sh
+++ tools/get_dlls.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 LIBRARY_LOADER=library-loader.exe
-LIBRARY_LOADER_PATH=$DIST
+LIBRARY_LOADER_PATH=$(readlink -f $DIST)
 DLLS_PATH="$GTK_INSTALL_PATH/bin"
 CWD=$(pwd)
 
-ALWAYS_NEEDED="libgcc_s_seh-1.dll libpixman-1-0.dll libpng16-16.dll zlib1.dll libepoxy-0.dll"
+ALWAYS_NEEDED="libgcc_s_seh-1.dll libpixman-1-0.dll libpng16-16.dll zlib1.dll libepoxy-0.dll libintl-8.dll libgmodule-2.0-0.dll"
 
 cd $DLLS_PATH
 cp $ALWAYS_NEEDED $LIBRARY_LOADER_PATH
