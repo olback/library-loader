79d0fd107823e862ed65590069fcc9e08b0e1f37
Edwin Svensson <git@olback.net>
2019-12-03 21:45:06 +0100

    fix missing dlls

--- tools/get_dlls.sh
+++ tools/get_dlls.sh
@@ -3,6 +3,13 @@
 LIBRARY_LOADER=library-loader.exe
 LIBRARY_LOADER_PATH=$DIST
 DLLS_PATH="$GTK_INSTALL_PATH/bin"
+CWD=$(pwd)
+
+ALWAYS_NEEDED="libgcc_s_seh-1.dll libpixman-1-0.dll libpng16-16.dll zlib1.dll libepoxy-0.dll"
+
+cd $DLLS_PATH
+cp $ALWAYS_NEEDED $LIBRARY_LOADER_PATH
+cd $CWD
 
 NEEDED=$(strings $LIBRARY_LOADER_PATH/$LIBRARY_LOADER | grep '\.dll$')
 
