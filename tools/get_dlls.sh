0616470a96caa52e25ae60de467a8d140f8ce19c
Edwin Svensson <git@olback.net>
2019-12-03 23:50:51 +0100

    [skip ci] remove quotes

--- tools/get_dlls.sh
+++ tools/get_dlls.sh
@@ -3,7 +3,7 @@
 LIBRARY_LOADER_PATH=$(readlink -f $DIST)
 DLLS_PATH="$GTK_INSTALL_PATH/bin"
 
-cp "$DLLS_PATH/*.dll" $LIBRARY_LOADER_PATH
+cp $DLLS_PATH/*.dll $LIBRARY_LOADER_PATH
 
 # CWD=$(pwd)
 # ALWAYS_NEEDED="libgcc_s_seh-1.dll libpixman-1-0.dll libpng16-16.dll zlib1.dll libepoxy-0.dll libintl-8.dll libgmodule-2.0-0.dll"
