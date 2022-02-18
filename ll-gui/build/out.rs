5d0ed9aff0a214a039fde90a4dfe68a2923819a7
Edwin Svensson <git@olback.net>
2022-02-18 19:58:23 +0100

    rust fmt

--- ll-gui/build/out.rs
+++ ll-gui/build/out.rs
@@ -3,12 +3,10 @@ use std::{fs, path::Path};
 const OUT_PATH: &str = "out";
 
 pub fn output_dir() {
-
     if Path::new(OUT_PATH).exists() {
         fs::remove_dir_all(OUT_PATH).unwrap();
         fs::create_dir_all(OUT_PATH).unwrap();
     } else {
         fs::create_dir_all(OUT_PATH).unwrap();
     }
-
 }
